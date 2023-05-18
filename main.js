6d8715b801ae5eca8068d134358952d28a2c72bd
DangSquare <qxdangvietanh@gmail.com>
2023-05-18 19:55:04 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+const navMenu=document.getElementById('nav-menu'),
+    navToggle=document.getElementById('nav-toggle'),
+    navClose=document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink=document.querySelectorAll('nav_link')
+
+function linkAction(){
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*Sevices*/
+const modalViews=document.querySelectorAll('.services_modal'),
+      modalBtns=document.querySelectorAll('.services_button'),
+      modalCloses=document.querySelectorAll('.services_modal-close')
+
+let modal=function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i)=> {
+    modalBtn.addEventListener('click', ()=>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose)=>{
+    modalClose.addEventListener('click', ()=>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+/*On top*/
+const sections=document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY=window.pageYOffset
+
+    sections.forEach(current=>{
+        const sectionHeight=current.offsetHeight
+        const sectionTop=current.offsetTop-50;
+        sectionId=current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*change background header*/
+function scrollHeader(){
+    const nav=document.getElementById('header')
+
+    if(this.scrollY >= 80) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scroll-header)
